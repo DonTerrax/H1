1b39373eae98f1bed408ea5fb2709a02f19e1bea
Mike Madsen <mike5171@zbc.dk>
2019-03-12 11:54:39 +0100

    update

---
+++ queue fifo/queue fifo/Game.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue_fifo
+{
+    class Game
+    {
+        private static Queue<Game> userGames = new Queue<Game>();
+        private static List<object> listOfGames = new List<object>();
+        private string title;
+        private int price;
+        private string platform;
+        private static int id;
+
+        #region properties
+
+        public Queue<Game> UserGames
+        {
+            get
+            {
+                return userGames;
+
+            }
+            set
+            {
+                userGames = value;
+
+            }
+        }
+
+        public List<object> ListOfGames
+        {
+            get
+            {
+                return listOfGames;
+
+            }
+            set
+            {
+                listOfGames = value; 
+
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+
+            }
+            set
+            {
+                title = value;
+
+            }
+        }
+
+        public int Price
+        {
+            get
+            {
+                return price;
+
+            }
+            set
+            {
+                price = value;
+
+            }
+        }
+
+        public string PlatForm
+        {
+            get
+            {
+                return platform;
+
+            }
+            set
+            {
+                platform = value;
+
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+
+            }
+            set
+            {
+                id = value;
+
+            }
+        }
+        #endregion
+
+        //Constructor
+        public Game(string title)
+        {
+            this.title = title;
+        }
+
+        public Game()
+        {
+
+        }
+
+        public static List<object>ReturnList()
+        {
+            return listOfGames;
+        }
+
+        //Constructor
+        public Game(string title,int price,string platform)
+        {
+            this.title = title;
+            this.price = price;
+            this.platform = platform;
+        }
+
+        //Add game
+        public void AddGame()
+        {
+            Game game = new Game(title,price,platform);
+            listOfGames.Add(game);
+        }
+        //Delete game
+        public void DeleteGame(int iid)
+        {
+            
+            listOfGames.RemoveAt(iid);
+        }
+
+        public int ShowNumberOfGames()
+        {
+            return listOfGames.Count;
+        }
+        //Find min value
+        public int Min()
+        {
+            List<int>min = new List<int>();
+            foreach (Game item in listOfGames)
+            {
+                min.Add(item.Price);
+            }
+
+            return min.Min();
+        }
+        //Find max value
+        public int Max()
+        {
+            List<int>max = new List<int>();
+            foreach (Game item in listOfGames)
+            {
+                max.Add(item.price);
+            }
+
+            return max.Max();
+        }
+
+        public string GamesAvalible(int menu)
+        {
+            foreach (Game item in Game.listOfGames)
+            {
+                if (listOfGames[menu] == item)
+                {
+                    return "ID:" + menu + " Title: " + item.Title + " Price: " + item.Price + " Platform: " + item.platform;
+                }
+                
+            }
+
+            return "empty";
+        }
+        //Find game
+        public string FindGame()
+        {
+            foreach (Game item in Game.listOfGames)
+            {
+                if (item.title.Contains(title))
+                {
+                    id++;
+                    return "title: " + item.title + "\nprice: "+ item.price+ "\nPlatform: "+item.PlatForm;
+                    
+                }
+            }
+
+            return "Game not found";
+        }
+
+        public Queue<Game> QueueGames()
+        {
+            foreach (Game item in listOfGames)
+            {
+                Game test = new Game(item.title,item.Price,item.platform);
+                userGames.Enqueue(test);
+                return userGames;
+            }
+
+            return userGames;
+        }
+       
+        
+    }
+}
---
+++ queue fifo/queue fifo/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue_fifo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int menu;
+            string title;
+            int price;
+            string platform;
+            int id;
+            while (true)
+            {
+                Console.WriteLine("1: Add game\n2: Delete game\n3: Show number of games\n4: Show min/max\n5: Find a game\n6: Show all games\n7: Buy game\n8: Exit");
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1://add game
+                        Console.WriteLine("Title:");
+                        title = Console.ReadLine();
+                        Console.WriteLine("Price:");
+                        price = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Platform:");
+                        platform = Console.ReadLine();
+                        Game gameAdd = new Game(title, price, platform);
+                        gameAdd.AddGame();
+                        Console.Clear();
+                        break;
+                    case 2://Delete game
+                        Console.WriteLine("Title:");
+                        title = Console.ReadLine();
+                        Game delete = new Game(title);
+
+                        Console.WriteLine("delete id:");
+                        int iid = int.Parse(Console.ReadLine());
+                        delete.DeleteGame(iid);
+                        break;
+                    case 3://show number of games
+                        Game showNumber = new Game();
+                        Console.WriteLine("Number of games {0}", showNumber.ShowNumberOfGames());
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    case 4://Show min max
+                        Game minMax = new Game();
+                        Console.WriteLine("Min: {0} Max: {1}",minMax.Min(),minMax.Max());
+                        break;
+                    case 5://Find game
+                        Console.WriteLine("Title: ");
+                        title = Console.ReadLine();
+
+                        Game search = new Game(title);
+                        Console.WriteLine(search.FindGame());
+                        Console.ReadKey();
+                        break;
+                    case 6://show all games
+                        menu = 0;
+                        foreach (Game items in Game.ReturnList())
+                        {
+                            Console.WriteLine("ID: {0} Title: {1}. Price: {2}. Platform: {3}", menu, items.Title, items.Price, items.PlatForm);
+                            menu++;
+                        }
+
+                        Console.ReadKey();
+                        break;
+                    case 7://Buy game
+                        Console.Clear();
+                        menu = 0;
+                        Game games = new Game();
+                        for (int i = 0; i < games.ListOfGames.Count; i++)
+                        {
+                            Console.WriteLine(games.GamesAvalible(menu));
+                            menu++;
+                        }
+
+                        Console.WriteLine("Choose a game: (title)");
+                        title = Console.ReadLine();
+
+                        Game buy = new Game();
+                        buy.QueueGames();
+                        Console.WriteLine("Games bought:");
+
+                        while (buy.UserGames.Count > 0)
+                        {
+                            Game name = buy.UserGames.Dequeue();
+                            Console.WriteLine("Title: {0}. Price: {1} Platform {2}", name.Title, name.Price,name.PlatForm);
+                        }
+                        Console.ReadKey();
+                        
+                        break;
+                    case 8:
+                        Environment.Exit(0);
+                        break; 
+                    default:
+                        break;
+
+                }
+            }
+        }
+    }
+}
