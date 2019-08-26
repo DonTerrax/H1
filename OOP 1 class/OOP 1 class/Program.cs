c8cfe63ea9e20140656a85f49bde95cd24bae0ce
Mike Madsen <mike5171@zbc.dk>
2019-08-26 12:00:36 +0200

    test

---
+++ OOP 1 class/OOP 1 class/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOP_1_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human mike = new Human("Madsen", "Mike", "Green", 36);
+            mike.introduceMySelf();
+
+            Human mischa = new Human("Enoksen", "Mischa", "brown", 27);
+            mischa.introduceMySelf();
+            
+        }
+    }
+}
---
+++ Udemy arrays/Udemy arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Udemy_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array2D = new int[,]
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+
+            Console.WriteLine("Central value is {0}",array2D[2,0]);
+        }
+    }
+}
